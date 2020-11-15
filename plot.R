4a67715c9ed7827d432525016644a33fd2fe00c7
Mark Butler <mrkcbutler@gmail.com>
2020-11-15 14:51:48 -0600

    Move plotting code into plot.R

--- plot.R
+++ plot.R
@@ -51,6 +51,41 @@ convert_to_labels <- function(condition_vec) {
 
 
 #############################################################################
+# Labels for payment variables.
+#############################################################################
+
+payment_colnames <- c('total_reimbursed', 'reimbursed_per_visit',
+                      'total_cost_of_claims', 'claim_cost_per_visit')
+payment_labels <- c(
+    'Total payment by insurance per patient',
+    'Mean payment by insurance per patient per visit',
+    'Total paid to hospitals per patient',
+    'Mean paid to hospital per patient per visit'
+)
+names(payment_labels) <- payment_colnames
+
+
+#############################################################################
+# Manipulate data frames for plotting.
+#############################################################################
+
+# Pivot a data frame of hospital visits, moving information about chronic
+# conditions into a single column.  Before the pivot, each hospital visit is
+# represented by a single row; after the pivot.  The resulting data frame is
+# used to generate a box plot that breaks down the distribution of a variable
+# by chronic condition.
+generate_condition_column <- function(data) {
+    data <- pivot_longer(data, all_of(chronic_conditions),
+                         names_to = 'condition', values_to = 'value') %>%
+        filter(value == 'Y') %>%
+        select(-value) %>%
+        # Modify the conditions names to improve plot readability.
+        mutate(condition = convert_to_labels(condition))
+    return(data)
+}
+
+
+#############################################################################
 # Format plot axes.
 #############################################################################
 
@@ -65,8 +100,10 @@ log_scale_dollar <- function(axis_label, axis) {
                                labels = label_dollar(largest_with_cents = -1))
         )
     } else if (axis == 'y') {
-        scale_y_continuous(axis_label, trans = trans, breaks = breaks,
-                           labels = label_dollar(largest_with_cents = -1))
+        return(
+            scale_y_continuous(axis_label, trans = trans, breaks = breaks,
+                               labels = label_dollar(largest_with_cents = -1))
+        )
     } else {
         stop('Argument "axis" must be "x" or "y".')
     }
@@ -173,18 +210,78 @@ plot_seasonality <- function(series_data, stl_model, title) {
 
 
 #############################################################################
-# Generate miscellaneous plots for the rendered markdown file.  These
-# functions avoid code repetition in the markdown file.
+# Generate miscellaneous plots for the rendered markdown file.
+#
+# These functions are defined in order to simplify the R markdown file.
 #############################################################################
 
-plot_claim_counts <- function(visit_type) {
+plot_chron_cond_claim_counts <- function(patients) {
+    for (column_name in chronic_conditions) {
+        condition_label <- chronic_condition_labels[column_name]
+        legend_title <- first_char_to_upper(condition_label)
+        condition_label <- str_replace_all(condition_label, '\n', ' ')
+        title_base <- str_c('Distribution of ', condition_label)
+
+        fig <- ggplot(patients, aes_string(x = 'claim_type', fill = column_name)) +
+            geom_bar(position = 'fill') +
+            scale_y_continuous('Percentage of patients',
+                               labels = label_percent()) +
+            scale_x_discrete(name = '') +
+            scale_fill_discrete(legend_title) +
+            ggtitle(title_base)
+        print(fig)
+
+        for (claim_type in claim_types) {
+            to_plot <- patients %>%
+                filter(claim_type == !!claim_type)
+            x_axis_values <- seq(min(to_plot$patient_claim_count),
+                                 max(to_plot$patient_claim_count))
+            title <- str_c(title_base, ', ', claim_type, 's')
+            fig <- to_plot %>%
+                ggplot(aes_string(x = 'patient_claim_count', fill = column_name)) +
+                geom_bar(position = 'fill') +
+                scale_x_discrete('Number of visits',
+                                 limits = factor(x_axis_values)) +
+                scale_fill_discrete(legend_title) +
+                ylab('Number of patients') +
+                ggtitle(title)
+            print(fig)
+        }
+    }
+}
+
+plot_payments <- function(patients) {
+    for (claim_type in claim_types) {
+        to_select <- c(payment_colnames, chronic_conditions)
+        to_plot <- patients %>%
+            filter(claim_type == !!claim_type) %>%
+            select(all_of(to_select)) %>%
+            # Patients for which the only visit has visit_cost == 0 show up with
+            # missing values for the columns in payment_colnames.
+            drop_na() %>%
+            generate_condition_column()
+        for (column_name in payment_colnames) {
+            title <- str_c(payment_labels[column_name], ', ', claim_type, 's')
+            fig <- ggplot(to_plot, aes_string(x = 'condition', y = column_name)) +
+                geom_boxplot(aes(fill = condition)) +
+                log_scale_dollar(axis_label = 'Payment amount', axis = 'y') +
+                xlab('Chronic condition') +
+                coord_flip() +
+                guides(fill = FALSE) +
+                ggtitle(title)
+            print(fig)
+        }
+    }
+}
+
+plot_provider_claim_counts <- function(claim_counts, claim_type) {
     plot_data <- claim_counts %>%
-        filter(visit_type == !!visit_type, claim_year == 2009)
+        filter(claim_type == !!claim_type, claim_year == 2009)
 
     to_plot <- plot_data %>%
         group_by(Provider) %>%
         summarise(count = sum(claim_count), .groups = 'drop')
-    title <- str_c('Number of claims per provider in 2009, ', visit_type, 's')
+    title <- str_c('Number of claims per provider in 2009, ', claim_type, 's')
     fig <- ggplot(to_plot, aes(x = count)) +
         geom_histogram(fill = 'navyblue', bins = 50) +
         xlab('Number of claims') +
@@ -192,13 +289,13 @@ plot_claim_counts <- function(visit_type) {
         ggtitle(title)
     print(fig)
 
-    cat('The maximum number of ', visit_type, ' claims for a provider is ',
+    cat('The maximum number of ', claim_type, ' claims for a provider is ',
         max(to_plot$count), '.\n', sep = '')
 
     to_plot <- plot_data %>%
         group_by(claim_month) %>%
         summarise(count = sum(claim_count), .groups = 'drop')
-    title <- str_c('Number of claims per month in 2009, ', visit_type, 's')
+    title <- str_c('Number of claims per month in 2009, ', claim_type, 's')
     fig <- ggplot(to_plot,
                   aes(x = claim_month, y = count)) +
         geom_col(fill = 'navyblue') +
@@ -214,7 +311,7 @@ plot_claim_counts <- function(visit_type) {
     return(datatable(
         to_display,
         rownames = FALSE,
-        colnames = c('Month', str_c('Max ', visit_type, ' claims per provider')),
+        colnames = c('Month', str_c('Max ', claim_type, ' claims per provider')),
         options = list(
             dom = 't',
             columnDefs = list(table_alignment),
@@ -232,22 +329,36 @@ plot_payment_distribution <- function(data, variable, title) {
     return(fig)
 }
 
+plot_fraction_original <- function(duplicates_data, claim_type) {
+    to_plot <- duplicates_data %>%
+        filter(claim_type == !!claim_type) %>%
+        filter_out_no_codes() %>%
+        group_by(Provider, PotentialFraud) %>%
+        summarise(
+            original_claims = sum(provider_is_original),
+            number_of_claims = n(),
+            .groups = 'drop'
+        ) %>%
+        mutate(fraction_original = original_claims / number_of_claims)
 
-#############################################################################
-# Manipulate data frames for plotting.
-#############################################################################
+    title <- str_c('Percentage of claims by provider that are original, ',
+                   claim_type, 's')
+    x_axis_label <- 'Percentage of claims by provider that are original'
+    fig_base <- ggplot(to_plot, aes(x = fraction_original)) +
+        scale_x_continuous(x_axis_label,
+                           labels = label_percent()) +
+        ggtitle(title)
+    plot_histograms(fig_base, y_label = 'providers', bins = 30)
 
-# Pivot a data frame of hospital visits, moving information about chronic
-# conditions into a single column.  Before the pivot, each hospital visit is
-# represented by a single row; after the pivot.  The resulting data frame is
-# used to generate a box plot that breaks down the distribution of a variable
-# by chronic condition.
-generate_condition_column <- function(data) {
-    data <- pivot_longer(data, all_of(chronic_conditions),
-                         names_to = 'condition', values_to = 'value') %>%
-        filter(value == 'Y') %>%
-        select(-value) %>%
-        # Modify the conditions names to improve plot readability.
-        mutate(condition = convert_to_labels(condition))
-    return(data)
+    title <- str_c('Number of claims vs percent that are original, ',
+                   claim_type, 's')
+    fig <- ggplot(to_plot,
+                  aes(x = fraction_original, y = number_of_claims)) +
+        geom_point(aes(color = PotentialFraud)) +
+        ylab('Number of claims by provider') +
+        scale_x_continuous(x_axis_label,
+                           labels = label_percent()) +
+        scale_color_discrete('Potential fraud') +
+        ggtitle(title)
+    print(fig)
 }
